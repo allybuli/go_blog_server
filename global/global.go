6de2643a2918eaa577da53e79e1e4922e4094256
Ally <allytd@126.com>
2025-04-18 01:21:51 +0800

    基于local_cache实现本地缓存

--- global/global.go
+++ global/global.go
@@ -5,14 +5,16 @@ import (
 
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/go-redis/redis"
+	"github.com/songzhibin97/gkit/cache/local_cache"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
 var (
-	Config *config.Config
-	Log    *zap.Logger
-	DB     *gorm.DB
-	Redis  redis.Client
-	Es     *elasticsearch.TypedClient
+	Config     *config.Config
+	Log        *zap.Logger
+	DB         *gorm.DB
+	Redis      redis.Client
+	Es         *elasticsearch.TypedClient
+	BlackCache local_cache.Cache
 )
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	global.Config = core.InitConf()
 	global.Log = core.InitLogger()
+	initialize.OtherInit()
 	global.DB = initialize.InitGorm()
 	global.Redis = initialize.ConnectRedis()
 	global.Es = initialize.ConnectEs()
