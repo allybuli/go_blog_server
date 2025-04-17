3972ee9360c12b6257e82149bc799cf1821cdf28
Ally <allytd@126.com>
2025-04-18 01:14:32 +0800

    main函数入口做各种初始化操作，构建对应的全局对象

--- global/global.go
+++ global/global.go
@@ -3,10 +3,16 @@ package global
 import (
 	"server/config"
 
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/go-redis/redis"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 var (
 	Config *config.Config
 	Log    *zap.Logger
+	DB     *gorm.DB
+	Redis  redis.Client
+	Es     *elasticsearch.TypedClient
 )
--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"server/core"
 	"server/global"
+	"server/initialize"
 )
 
 func main() {
 	global.Config = core.InitConf()
 	global.Log = core.InitLogger()
+	global.DB = initialize.InitGorm()
+	global.Redis = initialize.ConnectRedis()
+	global.Es = initialize.ConnectEs()
+
+	initialize.InitCron()
 
 	core.RunServer()
 }
